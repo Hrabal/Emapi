80bceb7422d6e11d774ada2164e2a41b0e02ae54
Federico Cerchiari <federico.cerchiari@quix.it>
2022-03-20 09:07:50 +0100

    black

--- setup.py
+++ setup.py
@@ -25,12 +25,12 @@ from emapi import __version__
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'emapi'
-DESCRIPTION = 'Event-Models based api framework.'
-URL = 'https://github.com/Hrabal/Emapi'
-EMAIL = 'federicocerchiari@gmail.com'
-AUTHOR = 'Federico Cerchiari'
-REQUIRES_PYTHON = '>=3.3'
+NAME = "emapi"
+DESCRIPTION = "Event-Models based api framework."
+URL = "https://github.com/Hrabal/Emapi"
+EMAIL = "federicocerchiari@gmail.com"
+AUTHOR = "Federico Cerchiari"
+REQUIRES_PYTHON = ">=3.3"
 VERSION = __version__
 
 REQUIRED = ["starlette", "tem-py", "tortoise-orm", "orjson", "PyYAML"]
@@ -41,68 +41,66 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 
 class UploadCommand(Command):
-    """Support setup.py upload."""
+	"""Support setup.py upload."""
 
-    description = 'Build and publish the package.'
-    user_options = []
+	description = "Build and publish the package."
+	user_options = []
 
-    @staticmethod
-    def status(s):
-        print(f'\033[1m{s}\033[0m')
+	@staticmethod
+	def status(s):
+		print(f"\033[1m{s}\033[0m")
 
-    def initialize_options(self):
-        pass
+	def initialize_options(self):
+		pass
 
-    def finalize_options(self):
-        pass
+	def finalize_options(self):
+		pass
 
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
+	def run(self):
+		try:
+			self.status("Removing previous builds…")
+			rmtree(os.path.join(here, "dist"))
+		except OSError:
+			pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+		self.status("Building Source and Wheel (universal) distribution…")
+		os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
 
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
+		self.status("Uploading the package to PyPI via Twine…")
+		os.system("twine upload dist/*")
 
-        self.status('Pushing git tags…')
-        os.system(f'git tag v{__version__}')
-        os.system('git push --tags')
+		self.status("Pushing git tags…")
+		os.system(f"git tag v{__version__}")
+		os.system("git push --tags")
 
-        sys.exit()
+		sys.exit()
 
 
 # Where the magic happens:
 setup(
-    name=NAME,
-    version=__version__,
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='APACHE 2.0',
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+	name=NAME,
+	version=__version__,
+	description=DESCRIPTION,
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	author=AUTHOR,
+	author_email=EMAIL,
+	python_requires=REQUIRES_PYTHON,
+	url=URL,
+	packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+	install_requires=REQUIRED,
+	extras_require=EXTRAS,
+	include_package_data=True,
+	license="APACHE 2.0",
+	classifiers=[
+		"License :: OSI Approved :: Apache Software License",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: Implementation :: CPython",
+		"Programming Language :: Python :: Implementation :: PyPy",
 		"Topic :: Internet :: WWW/HTTP :: HTTP Servers",
 		"Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
-    ],
-    cmdclass={
-        'upload': UploadCommand,
-    },
+	],
+	cmdclass={"upload": UploadCommand,},
 )
